7f8894976cd35bcc3a8ea8afc849b76b8113a76d
leif.nevener <leif.nevener@us.af.mil>
2024-02-09 22:19:43 -0600

    first commit for nertz scorecard application

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Enter Participants</h1>
+      <input autoFocus type="text"></input>
+    </>
+  )
+}
+
+export default App
