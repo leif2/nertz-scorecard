f866fd796fa3be7532e3d055a6e0b6938691def9
leif.nevener <leif.nevener@us.af.mil>
2024-02-10 09:06:03 -0600

    added onKeyUp and onChange to input

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [names, setNames] = useState<string[]>([]);
+
+  const handleOnChange = (event: any) => {
+    setValue(event?.target.value);
+  }
+
+  const handleOnKeyUp = (event: any) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      console.log(value);
+      const namesList = [...names, value];
+      setNames(namesList);
+      console.log(names);
+      event.currentTarget.value = "";
+    }
+  }
 
   return (
     <>
+    <div>{names}</div>
       <h1>Enter Participants</h1>
-      <input autoFocus type="text"></input>
+      <input autoFocus type="text" onKeyUp={handleOnKeyUp} onChange={handleOnChange} placeholder='Press Enter to Save the name'></input>
     </>
   )
 }
