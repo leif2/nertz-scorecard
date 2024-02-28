11a9c53c8afc1e98830cebb260441cf15ec2b89e
leif.nevener <leif.nevener@us.af.mil>
2024-02-27 22:38:12 -0600

    added useEffect to show start and reset buttons, also added divs mapping for each name added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [value, setValue] = useState<string>("");
   const [names, setNames] = useState<string[]>([]);
+  const [showMenuButtons, setShowMenuButtons] = useState<boolean>(false);
 
   const handleOnChange = (event: any) => {
     setValue(event?.target.value);
@@ -20,9 +21,24 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    if (names.length > 0) {
+      setShowMenuButtons(true);
+    }
+  }, [names]);
+
+  const renderNameList = (names: string[]) => {
+    return names?.map((name: string, index) => <div key={index}>{name}</div>)
+  }
+
   return (
     <>
-    <div>{names}</div>
+      <div>{renderNameList(names)}</div>
+      {showMenuButtons &&
+        <>
+          <div><button>Start Game</button></div>
+          <div><button>Reset</button></div>
+        </>}
       <h1>Enter Participants</h1>
       <input autoFocus type="text" onKeyUp={handleOnKeyUp} onChange={handleOnChange} placeholder='Press Enter to Save the name'></input>
     </>
